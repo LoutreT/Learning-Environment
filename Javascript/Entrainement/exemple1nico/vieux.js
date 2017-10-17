5ca7e04c6eee4bb558097b66a33ce62ae99b7c5b
Vincent <v.nassaux@nootos.com>
2017-10-17 12:57:19 +0200

    reorg

--- Javascript/Entrainement/exemple1nico/vieux.js
+++
@@ -1,5 +0,0 @@
-var agemax = prompt("Jusque quel âge pensez vous vivre?");
-var age = prompt(" Quel age avez vous?");
-var repas = prompt("combien de fois mangez vous par jours?");
-
-document.write("Il vous reste : " + (((agemax - age)* 365)* repas) + " repas. Profitez bien. :^)")
