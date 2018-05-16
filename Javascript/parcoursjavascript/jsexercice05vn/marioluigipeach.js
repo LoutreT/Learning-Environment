c01e97247d66d0aee812c81d8bec6565fa9e055d
Vincent <v.nassaux@nootos.com>
2018-05-16 04:35:56 +0200

    RetrouvailleDeDossiers

--- Javascript/parcoursjavascript/jsexercice05vn/marioluigipeach.js
+++
@@ -1,6 +0,0 @@
-let perso=["Mario","Luigi","peach"];
-console.log(perso)*2;
-document.write(perso+"<br>");
-perso.push("Bowser");
-console.log(perso);
-document.write(perso);
--- Javascript/parcoursjavascript/jsexercice05vn/script.js
+++
@@ -1,9 +0,0 @@
-var add=0;
-let chiffre=[1,2,3,4,5,6,7,8,9];
-for (let i=0; i<9; i++){
-  add = add+chiffre[i];
-  console.log(add-chiffre[i]+" + "+chiffre[i]);
-  console.log(add);
-  document.write(add-chiffre[i]+" + "+chiffre[i]+"<br>");
-  document.write(add+"<br>");
-}
